beb2c9e360e374287f4fdbf9783e6cf3e6b5764a
Raphael Wanjiku <phaelgi@gmail.com>
2021-03-13 19:57:12 +0300

    images added

--- Master2.py
+++ Master2.py
@@ -1,6 +1,8 @@
 import MatcherFolder as mt
 import pandas as pd
+from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 # set the training dictionary
@@ -82,3 +84,27 @@ for _ in range(3):
 # # training and validation steps
 training_steps = training_dataset.n // training_dataset.batch_size
 validation_steps = validation_dataset.n // validation_dataset.batch_size
+
+
+# using functional api instead of sequential -more flexible
+inputs=keras.Input(shape=(28*28))
+x=layers.Dense(512,activation='relu',name='first_layer')(inputs)
+x=layers.Dense(256,activation='relu',name='second_layer')(x)
+output=layers.Dense(10,activation='softmax')(x)
+model=keras.Model(inputs=inputs,outputs=output)
+
+#configure the training part of the network
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False), #logits to true if sequential
+    optimizer=keras.optimizers.Adam(lr=0.001),
+    #add from logits since there was not softmax function in the dense classification layer
+    metrics=['accuracy'],
+
+)
+#then train the network
+#model.fit(training_dataset,batch_size=32,epochs=5,verbose=2)
+
+#then evaluate the model
+#model.evaluate(x_test,y_test,batch_size=32,verbose=2)
+
+print(training_dataset.classes)
