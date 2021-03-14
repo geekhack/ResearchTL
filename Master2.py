8612e72521190937e58b333fef4a11b60f6a2c38
Raphael Wanjiku <phaelgi@gmail.com>
2021-03-14 12:11:42 +0300

    model pushed

--- Master2.py
+++ Master2.py
@@ -1,5 +1,7 @@
 import MatcherFolder as mt
 import pandas as pd
+import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
@@ -38,20 +40,20 @@ training_dataset = train_dataset_gen.flow_from_dataframe(dataframe=train_df,
                                                          shuffle=True,
                                                          batch_size=BATCH_SIZE,
                                                          target_size=IMG_SIZE,
-                                                         class_mode="categorical",
+                                                         class_mode="sparse",
                                                          subset="training")
 
 validation_dataset = validation_dataset_gen.flow_from_dataframe(dataframe=validation_df,
-                                                         directory="./Data/validate/",
-                                                         x_col="file",
-                                                         y_col="label",
-                                                         shuffle=True,
-                                                         batch_size=BATCH_SIZE,
-                                                         target_size=IMG_SIZE,
-                                                         class_mode="categorical",
-                                                         subset=None)
+                                                                directory="./Data/validate/",
+                                                                x_col="file",
+                                                                y_col="label",
+                                                                shuffle=True,
+                                                                batch_size=BATCH_SIZE,
+                                                                target_size=IMG_SIZE,
+                                                                class_mode="sparse",
+                                                                subset=None)
 # show the classes for both training and validation sets
-print(training_dataset.class_indices)
+print(list(training_dataset.class_indices.keys()))
 print(validation_dataset.class_indices)
 
 # show the images filenames
@@ -63,9 +65,12 @@ print(validation_dataset.filenames)
 # loop through the training images
 
 print("***************Training images***************")
-for _ in range(3):
+# get the labels
+t_labels = []
+for _ in range(len(training_dataset.filenames)):
     image, label = training_dataset.next()
     # check if the image shapes match those of resnet 50
+    t_labels.append(label)
     print(image.shape)
     # display the first image from the iterator
     # plt.imshow(image[0])
@@ -73,38 +78,39 @@ for _ in range(3):
 
 # loop through the validation images
 print("***************Validation images***************")
-for _ in range(3):
+v_labels = []
+for _ in range(len(validation_dataset.filenames)):
     image, label = validation_dataset.next()
     # check if the image shapes match those of resnet 50
+    v_labels.append(label)
     print(image.shape)
     # display the first image from the iterator
     # plt.imshow(image[0])
     # plt.show()
-#
+
 # # training and validation steps
 training_steps = training_dataset.n // training_dataset.batch_size
 validation_steps = validation_dataset.n // validation_dataset.batch_size
 
-
 # using functional api instead of sequential -more flexible
-inputs=keras.Input(shape=(28*28))
-x=layers.Dense(512,activation='relu',name='first_layer')(inputs)
-x=layers.Dense(256,activation='relu',name='second_layer')(x)
-output=layers.Dense(10,activation='softmax')(x)
-model=keras.Model(inputs=inputs,outputs=output)
-
-#configure the training part of the network
+inputs = keras.Input(shape=(28, 28, 3))
+x = layers.Dense(512, activation='relu', name='first_layer')(inputs)
+x = layers.Dense(256, activation='relu', name='second_layer')(x)
+x = layers.Flatten()(x)
+output = layers.Dense(10, activation='softmax')(x)
+model = keras.Model(inputs=inputs, outputs=output)
+print(model.summary())
+# configure the training part of the network
 model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False), #logits to true if sequential
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),  # logits to true if sequential
     optimizer=keras.optimizers.Adam(lr=0.001),
-    #add from logits since there was not softmax function in the dense classification layer
-    metrics=['accuracy'],
+    # add from logits since there was not softmax function in the dense classification layer
+    metrics=['sparse_categorical_accuracy','mse'],
 
 )
-#then train the network
-#model.fit(training_dataset,batch_size=32,epochs=5,verbose=2)
-
-#then evaluate the model
-#model.evaluate(x_test,y_test,batch_size=32,verbose=2)
-
-print(training_dataset.classes)
+# then train the network
+model.fit(training_dataset, batch_size=32, epochs=5, verbose=2)
+print(model.summary())
+# then evaluate the model
+model.evaluate(validation_dataset,batch_size=32,verbose=2)
+print(model.summary())
